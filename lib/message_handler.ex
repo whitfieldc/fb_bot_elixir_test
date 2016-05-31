32f755f9a43d70591b5c85968a302b23a802864b
Charles Whitfield <whitfield.cw@gmail.com>
2016-05-31 11:29:47 -0700

    add postback handling

--- lib/message_handler.ex
+++ lib/message_handler.ex
@@ -11,9 +11,30 @@ defmodule Fbtestapp.MessageHandler do
 
     send_button_message(msg.sender.id, "Choose from the following options", buttons)
   end
+
+  def handle(msg = %{postback: %{payload: "PB_NAME"}}) do
+    send_text_message(
+      msg.sender.id,
+      "My name is SST Laboratories Siege Automation E54, otherwise known as Bastion"
+    )
+  end
   
   def handle(msg) do
     Logger.info "Unhandled message:\n#{inspect msg}"
+  end
+
+  defp send_text_message(recipient, text) do
+    payload = %{
+      recipient: %{id: recipient},
+
+      message: %{
+        text: text
+      }
+    }
+
+    url: = "https://graph.facebook.com/v2.6/me/messages?access_token=#{@fb_page_access_token}"
+    headers = [{"Content-Type", "application/json"}]
+    HTTPoison.post!(url, Poison.encode!(payload), headers)
   end
 
   defp send_button_message(recipient, text, buttons) do

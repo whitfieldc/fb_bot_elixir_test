1c97db9e53849efd5b6faaf4304bbed71f53510a
Charles Whitfield <whitfield.cw@gmail.com>
2016-05-30 18:51:15 -0700

    complete message handler module

--- lib/message_handler.ex
+++ lib/message_handler.ex
@@ -1,11 +1,35 @@
 defmodule Fbtestapp.MessageHandler do
   require Logger
 
+  @fb_page_access_token System.get_env("FB_PAGE_ACCESS_TOKEN") 
+
   def handle(msg = %{message: %{text: _text}}) do
-    # incoming text message
+    buttons = [
+      %{type: "postback", title: "Your name", payload: "PB_NAME"},
+      %{type: "web_url", title: "Your website", url: "https://https://playoverwatch.com/en-us/heroes/bastion/"}
+    ]
+
+    send_button_message(msg.sender.id, "Choose from the following options", buttons)
   end
 
-  def handle(msg) do
-    Logger.info "Unhandled message:\n#{inspect msg}"
+  defp send_button_message(recipient, text, buttons) do
+    payload = %{
+      recipient: %{id: recipient},
+
+      message: %{
+        attachment: %{
+          type: "template",
+          payload: %{
+            template_type: "button",
+            text: text,
+            buttons: buttons
+          }
+        }
+      }
+    }
+
+    url = "https://graph.facebook.com/v2.6/me/messages?access_token=#{@fb_page_access_token}"
+    headers = [{"Content-Type", "application/json"}]
+    HTTPoison.post!(url, Poison.encode!(payload), headers)
   end
 end

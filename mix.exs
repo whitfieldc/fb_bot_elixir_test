e1f6d9c14d174c3b6ceb8b0a5323f4ab2607a50a
Charles Whitfield <whitfield.cw@gmail.com>
2016-05-30 18:34:53 -0700

    add httpoison lib to deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Fbtestapp.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :cowboy, :plug],
+    [applications: [:logger, :cowboy, :plug, :httpoison],
      mod: {Fbtestapp, []}]
   end
 
@@ -29,6 +29,8 @@ defmodule Fbtestapp.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [ {:cowboy, "~> 1.0"},
-      {:plug, "~> 1.0"}]
+      {:plug, "~> 1.0"},
+      {:poison, "~> 2.1"},
+      {:httpoison, "~> 0.8.3"}]
   end
 end
